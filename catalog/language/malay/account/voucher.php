d8e84dc3d35a1b6ab2df1ec4192ff7dc18ca41bd
Azri Jamil <wajatimur@live.com>
2013-10-28 06:10:26 +0800

    Merge with catalog

---
+++ catalog/language/malay/account/voucher.php
@@ -0,0 +1,29 @@
+<?php
+// Heading 
+$_['heading_title']    = 'Pembelian Baucer Hadiah';
+
+// Text
+$_['text_account']     = 'Akaun';
+$_['text_voucher']     = 'Baucer Hadiah';
+$_['text_description'] = 'Baucer hadiah ini akan diemail kepada penerima setelah pembayaran lengkap';
+$_['text_agree']       = 'Saya faham bahawa baucer ini tidak boleh dipulangkan.';
+$_['text_message']     = '<p>Terima kasih kerana membeli baucer hadiah! Penerima baucer akan menerima baucer hadiah ini beserta cara menebusnya setelah pembayaran dilengkapkan.</p>';
+$_['text_for']         = '%s Baucer hadiah untuk %s';
+
+// Entry
+$_['entry_to_name']    = 'Nama Penerima:';
+$_['entry_to_email']   = 'Email Penerima:';
+$_['entry_from_name']  = 'Nama Anda:';
+$_['entry_from_email'] = 'Email Anda:';
+$_['entry_theme']      = 'Tema Baucer Hadiah:';
+$_['entry_message']    = 'Mesej:<br /><span class="help">(Optional)</span>';
+$_['entry_amount']     = 'Jumlah:<br /><span class="help">(Nilai antara %s dan %s)</span>';
+
+// Error
+$_['error_to_name']    = 'Nama penerima antara 1 dan 64 aksara!';
+$_['error_from_name']  = 'Nama anda antara 1 dan 64 aksara!';
+$_['error_email']      = 'Alamat email tidak sah!';
+$_['error_theme']      = 'Anda mesti pilih tema!';
+$_['error_amount']     = 'Jumlah mestilah antara %s dan %s!';
+$_['error_agree']      = 'Amaran: Anda mesti bersetuju bahawa baucer hadiah ini tidak boleh dipulangkan!';
+?>
---
+++ catalog/language/malay/checkout/manual.php
@@ -0,0 +1,29 @@
+<?php
+// Text
+$_['text_for']          = '%s Baucer Hadiah untuk %s';
+$_['text_success']      = 'Jumlah pesanan telah berjaya diulangkira!';
+
+// Error
+$_['error_warning']     = 'Amaran: Sila semak borang dengan terperinci!';
+$_['error_permission']  = 'Anda tiada kebenaran untuk akses ke laman ini, sila rujuk kepada pentadbir sistem.';
+$_['error_customer']    = 'Amaran: Pelanggan yang dipilih tidak dijumpai!';
+$_['error_email']       = 'Alamat email tidak sah!';
+$_['error_postcode']    = 'Poskod mesti antara 2 dan 10 aksara!';
+$_['error_country']     = 'Sila pilih negara!';
+$_['error_zone']        = 'Sila pilih zone/negeri!';
+$_['error_stock']       = 'Produk yang bertanda *** samada tidak ada atau kuantiti yang dihajati tidak mencukupi!';
+$_['error_minimum']     = 'Pesanan minimum untuk %s ialah %s!';	
+$_['error_required']    = '%s diperlukan!';
+$_['error_shipping']    = 'Amaran: Kaedah penghantran diperlukan!';
+$_['error_no_shipping'] = 'Amaran: Tiada pilihan penghantaran tersedia!';
+$_['error_payment']     = 'Amaran: Kaedah pembayaran diperlukan!';
+$_['error_no_payment']  = 'Amaran: Tiada pilihan pembayaran tersedia!';
+$_['error_coupon']      = 'Amaran: Samada kupon tidak sah, luput tarikh atau telah mencapai had penggunaan!';
+$_['error_voucher']     = 'Amaran: Samada baucer hadiah tidak sah atau keseluruhan baki telah digunakan!';
+$_['error_reward']      = 'Amaran: Sila masukkan jumlah mata ganjaran yang ingin digunakan!';	
+$_['error_points']      = 'Amaran: Anda tidak mempunyai %s mata ganjaran!';
+$_['error_maximum']     = 'Amaran: Jumlah maksimum mata ganjaran yang boleh digunakan ialah %s!';
+$_['error_to_name']     = 'Nama penerima mestilah antara 1 dan 64 aksara!';
+$_['error_from_name']   = 'Nama anda mestilah antara 1 dan 64 aksara!';
+$_['error_amount']      = 'Jumlah mestilah antara %s dan %s!';
+?>
